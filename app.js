17144ff5ac505daa4f2b12c766bbdb2936a8deaf
Gabriel4585 <gagarcia@chapman.edu>
2023-04-07 10:19:37 -0700

    added middle section and button listener but not working

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-/**Maybe add Javascript? */
+/*FIXME buttons not recognizing the activity*/
+var buttons = document.querySelectorAll('.button');
+var pressedButtonName = document.getElementById('Button-pressed1');
+var middleSection = document.getElementById('middle-section');
+// Add click event listener to each button
+buttons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        var buttonText = button.textContent;
+        pressedButtonName.textContent = "You clicked ".concat(buttonText);
+    });
+});
---
+++ app.ts
@@ -0,0 +1,12 @@
+/*FIXME buttons not recognizing the activity*/
+const buttons = document.querySelectorAll('.button');
+const pressedButtonName = document.getElementById('Button-pressed1');
+const middleSection = document.getElementById('middle-section');
+
+// Add click event listener to each button
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const buttonText = button.textContent;
+    pressedButtonName.textContent = `You clicked ${buttonText}`;
+  });
+});
