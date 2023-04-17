1bba43da48b51ec65d2d27cfefa86ef93bb32c50
Gabriel4585 <gagarcia@chapman.edu>
2023-04-16 20:52:10 -0700

    added small and medium ranged weapon logic for both attack and defense

--- app.js
+++ app.js
@@ -5,27 +5,46 @@ var Spear_Attack_Value = 1;
 var Spear_Defense_Value = 3;
 var Spear_Bonus_Against_Mounted = 3;
 var Mounted_Attack_Value = 4;
-var Mounted_Defense_Value = 4;
+var Mounted_Melee_Defense_Value = 4;
+var Mounted_Ranged_Defense_Value = 2;
+var Small_Ranged_Attack_Value = 3;
+var Small_Ranged_Defense_Value = -1;
+var Small_Ranged_Free = 1;
+var Medium_Ranged_Attack_Value = 4;
+var Medium_Ranged_Defense_Value = -4;
+var Medium_Ranged_Free = 1;
+/*----------------- Bonus Calculations----------------- */
+/* Interactions calculated on the attacker portion */
 document.addEventListener('DOMContentLoaded', function () {
     var netStrengthParagraph = document.getElementById('net-strength');
     var totalAttackBonus = document.getElementById('attacker-total-strength');
     var totalDefenseBonus = document.getElementById('defender-total-strength');
+    var rangeActual = document.getElementById('rangeActual');
     var attackerCheckboxes = document.querySelectorAll('.attacker input[type="checkbox"]');
     var defenderCheckboxes = document.querySelectorAll('.defender input[type="checkbox"]');
     function updateNetStrength() {
         var netStrength = 0;
         var attackerStrength = 0;
         var defenderStrength = 0;
+        var range = Number(rangeActual.value);
         attackerCheckboxes.forEach(function (checkbox) {
             if (checkbox.checked) {
                 if (checkbox.id === 'attackHelmet') {
                     attackerStrength += Helmet_Attack_Value;
                 }
-                else if (checkbox.id === 'attackSpear') {
+                else if (checkbox.id === 'attackSpear') { /*melee weapon */
                     attackerStrength += Spear_Attack_Value;
                     var defenseMountedCheckbox = document.getElementById('defenseMounted');
                     if (defenseMountedCheckbox.checked) { /*checks if the opposing mounted is already checked */
                         attackerStrength += Spear_Bonus_Against_Mounted;
+                    }
+                    var defenseSmallRangedCheckbox_1 = document.getElementById('defenseSmallGun');
+                    var defenseMediumRangedCheckbox_1 = document.getElementById('defenseMediumGun');
+                    if (defenseSmallRangedCheckbox_1.checked) { /*checks if the opposing small ranged is already checked */
+                        defenderStrength += Small_Ranged_Defense_Value;
+                    }
+                    if (defenseMediumRangedCheckbox_1.checked) { /*checks if the opposing medium ranged is already checked */
+                        defenderStrength += Medium_Ranged_Defense_Value;
                     }
                 }
                 else if (checkbox.id === 'attackMounted') {
@@ -35,19 +54,43 @@ document.addEventListener('DOMContentLoaded', function () {
                         defenderStrength += Spear_Bonus_Against_Mounted;
                     }
                 }
+                else if (checkbox.id === 'attackSmallGun') { /*ranged weapon */
+                    if (range <= Small_Ranged_Free) {
+                        attackerStrength += Small_Ranged_Attack_Value;
+                    }
+                    else {
+                        attackerStrength += (Small_Ranged_Attack_Value - (range - Small_Ranged_Free));
+                    }
+                }
+                else if (checkbox.id === 'attackMediumGun') { /*ranged weapon */
+                    if (range <= Medium_Ranged_Free) {
+                        attackerStrength += Medium_Ranged_Attack_Value;
+                    }
+                    else {
+                        attackerStrength += (Medium_Ranged_Attack_Value - (range - Medium_Ranged_Free));
+                    }
+                }
             }
         });
         /* All special button interactions handled in attacker section*/
         defenderCheckboxes.forEach(function (checkbox) {
+            var attackSpearCheckbox = document.getElementById('attackSpear');
             if (checkbox.checked) {
                 if (checkbox.id === 'defenseHelmet') {
                     defenderStrength += Helmet_Defense_Value;
                 }
                 else if (checkbox.id === 'defenseSpear') {
-                    defenderStrength += Spear_Defense_Value;
+                    if (attackSpearCheckbox.checked) { /*Should only get defense value if in melee combat */
+                        defenderStrength += Spear_Defense_Value;
+                    }
                 }
                 else if (checkbox.id === 'defenseMounted') {
-                    defenderStrength += Mounted_Defense_Value;
+                    if (attackSpearCheckbox.checked) { /*defensive bonus different if in melee combat */
+                        defenderStrength += Mounted_Melee_Defense_Value;
+                    }
+                    else {
+                        defenderStrength += Mounted_Ranged_Defense_Value;
+                    }
                 }
             }
         });
@@ -62,31 +105,60 @@ document.addEventListener('DOMContentLoaded', function () {
     defenderCheckboxes.forEach(function (checkbox) {
         checkbox.addEventListener('click', updateNetStrength);
     });
+    rangeActual.addEventListener('input', updateNetStrength);
 });
-var rangedCheckbox = document.getElementById("attackRanged");
-var smallRangedCheckbox = document.getElementById("attackSmallGun");
-smallRangedCheckbox.style.display = "none"; /*hide the small ranged checkbox by default */
-var smallRangedLabel = document.querySelector('label[for="attackSmallGun"]');
-smallRangedLabel.style.display = "none"; /*hide the small ranged label by default */
-var mediumRangedCheckbox = document.getElementById("attackMediumGun");
-mediumRangedCheckbox.style.display = "none"; /*hide the medium ranged checkbox by default */
-var mediumRangedLabel = document.querySelector('label[for="attackMediumGun"]');
-mediumRangedLabel.style.display = "none"; /*hide the medium ranged label by default */
-rangedCheckbox.addEventListener("change", function () {
+/* ----------------------Reapearing Elements------------------------------- */
+var attackRangedCheckbox = document.getElementById("attackRanged");
+var defenseRangedCheckbox = document.getElementById("defenseRanged");
+var attackSmallRangedCheckbox = document.getElementById("attackSmallGun");
+attackSmallRangedCheckbox.style.display = "none"; /*hide the small ranged checkbox by default */
+var AttackSmallRangedLabel = document.querySelector('label[for="attackSmallGun"]');
+AttackSmallRangedLabel.style.display = "none"; /*hide the small ranged label by default */
+var attackMediumRangedCheckbox = document.getElementById("attackMediumGun");
+attackMediumRangedCheckbox.style.display = "none"; /*hide the medium ranged checkbox by default */
+var attackMediumRangedLabel = document.querySelector('label[for="attackMediumGun"]');
+attackMediumRangedLabel.style.display = "none"; /*hide the medium ranged label by default */
+var defenseSmallRangedCheckbox = document.getElementById("defenseSmallGun");
+defenseSmallRangedCheckbox.style.display = "none"; /*hide the small ranged checkbox by default */
+var defenseSmallRangedLabel = document.querySelector('label[for="defenseSmallGun"]');
+defenseSmallRangedLabel.style.display = "none"; /*hide the small ranged label by default */
+var defenseMediumRangedCheckbox = document.getElementById("defenseMediumGun");
+defenseMediumRangedCheckbox.style.display = "none"; /*hide the medium ranged checkbox by default */
+var defenseMediumRangedLabel = document.querySelector('label[for="defenseMediumGun"]');
+defenseMediumRangedLabel.style.display = "none"; /*hide the medium ranged label by default */
+attackRangedCheckbox.addEventListener("change", function () {
     if (this.checked) {
-        smallRangedCheckbox.style.display = "inline-block";
-        smallRangedCheckbox.checked = false;
-        smallRangedLabel.style.display = "inline-block";
-        mediumRangedCheckbox.style.display = "inline-block";
-        mediumRangedCheckbox.checked = false;
-        mediumRangedLabel.style.display = "inline-block";
+        attackSmallRangedCheckbox.style.display = "inline-block";
+        attackSmallRangedCheckbox.checked = false;
+        AttackSmallRangedLabel.style.display = "inline-block";
+        attackMediumRangedCheckbox.style.display = "inline-block";
+        attackMediumRangedCheckbox.checked = false;
+        attackMediumRangedLabel.style.display = "inline-block";
     }
     else {
-        smallRangedCheckbox.style.display = "none";
-        smallRangedCheckbox.checked = false;
-        smallRangedLabel.style.display = "none";
-        mediumRangedCheckbox.style.display = "none";
-        mediumRangedCheckbox.checked = false;
-        mediumRangedLabel.style.display = "none";
+        attackSmallRangedCheckbox.style.display = "none";
+        attackSmallRangedCheckbox.checked = false;
+        AttackSmallRangedLabel.style.display = "none";
+        attackMediumRangedCheckbox.style.display = "none";
+        attackMediumRangedCheckbox.checked = false;
+        attackMediumRangedLabel.style.display = "none";
+    }
+});
+defenseRangedCheckbox.addEventListener("change", function () {
+    if (this.checked) {
+        defenseSmallRangedCheckbox.style.display = "inline-block";
+        defenseSmallRangedCheckbox.checked = false;
+        defenseSmallRangedLabel.style.display = "inline-block";
+        defenseMediumRangedCheckbox.style.display = "inline-block";
+        defenseMediumRangedCheckbox.checked = false;
+        defenseMediumRangedLabel.style.display = "inline-block";
+    }
+    else {
+        defenseSmallRangedCheckbox.style.display = "none";
+        defenseSmallRangedCheckbox.checked = false;
+        defenseSmallRangedLabel.style.display = "none";
+        defenseMediumRangedCheckbox.style.display = "none";
+        defenseMediumRangedCheckbox.checked = false;
+        defenseMediumRangedLabel.style.display = "none";
     }
 });
--- app.ts
+++ app.ts
@@ -5,13 +5,22 @@ const Spear_Attack_Value = 1;
 const Spear_Defense_Value = 3;
 const Spear_Bonus_Against_Mounted = 3;
 const Mounted_Attack_Value = 4;
-const Mounted_Defense_Value = 4;
+const Mounted_Melee_Defense_Value = 4;
+const Mounted_Ranged_Defense_Value = 2;
+const Small_Ranged_Attack_Value = 3;
+const Small_Ranged_Defense_Value = -1;
+const Small_Ranged_Free = 1;
+const Medium_Ranged_Attack_Value = 4;
+const Medium_Ranged_Defense_Value = -4;
+const Medium_Ranged_Free = 1;
 
 /*----------------- Bonus Calculations----------------- */
+/* Interactions calculated on the attacker portion */
 document.addEventListener('DOMContentLoaded', () => {
     const netStrengthParagraph = document.getElementById('net-strength');
     const totalAttackBonus = document.getElementById('attacker-total-strength');
     const totalDefenseBonus = document.getElementById('defender-total-strength');
+    const rangeActual = document.getElementById('rangeActual') as HTMLInputElement;
   
     const attackerCheckboxes = document.querySelectorAll('.attacker input[type="checkbox"]') as NodeListOf<HTMLInputElement>;
     const defenderCheckboxes = document.querySelectorAll('.defender input[type="checkbox"]') as NodeListOf<HTMLInputElement>;
@@ -20,15 +29,24 @@ document.addEventListener('DOMContentLoaded', () => {
       let netStrength = 0;
       let attackerStrength = 0;
       let defenderStrength = 0;
+      let range = Number(rangeActual.value);
       attackerCheckboxes.forEach((checkbox) => {
         if (checkbox.checked) {
             if(checkbox.id === 'attackHelmet'){
                 attackerStrength += Helmet_Attack_Value;
-            }else if(checkbox.id === 'attackSpear'){
+            }else if(checkbox.id === 'attackSpear'){ /*melee weapon */
                 attackerStrength += Spear_Attack_Value;
                 const defenseMountedCheckbox = document.getElementById('defenseMounted') as HTMLInputElement;
                 if(defenseMountedCheckbox.checked){ /*checks if the opposing mounted is already checked */
-                    attackerStrength += Spear_Bonus_Against_Mounted;
+                  attackerStrength += Spear_Bonus_Against_Mounted;
+                }
+                const defenseSmallRangedCheckbox = document.getElementById('defenseSmallGun') as HTMLInputElement;
+                const defenseMediumRangedCheckbox = document.getElementById('defenseMediumGun') as HTMLInputElement;
+                if(defenseSmallRangedCheckbox.checked){ /*checks if the opposing small ranged is already checked */
+                  defenderStrength += Small_Ranged_Defense_Value;
+                }
+                if(defenseMediumRangedCheckbox.checked){ /*checks if the opposing medium ranged is already checked */
+                  defenderStrength += Medium_Ranged_Defense_Value;
                 }
             }else if(checkbox.id === 'attackMounted'){
                 attackerStrength += Mounted_Attack_Value;
@@ -36,19 +54,39 @@ document.addEventListener('DOMContentLoaded', () => {
                 if(defenseSpearCheckbox.checked){   /*checks if the opposing spear is already checked */
                     defenderStrength += Spear_Bonus_Against_Mounted;
                 }
+            }else if(checkbox.id === 'attackSmallGun'){ /*ranged weapon */
+              if(range <= Small_Ranged_Free){
+                attackerStrength += Small_Ranged_Attack_Value;
+              }else{
+                attackerStrength += (Small_Ranged_Attack_Value - (range-Small_Ranged_Free));
+              }
+            }else if(checkbox.id === 'attackMediumGun'){  /*ranged weapon */
+              if(range <= Medium_Ranged_Free){
+                attackerStrength += Medium_Ranged_Attack_Value;
+              }else{
+                attackerStrength += (Medium_Ranged_Attack_Value - (range-Medium_Ranged_Free));
+              }
+
             }
         }
       });
 
       /* All special button interactions handled in attacker section*/
       defenderCheckboxes.forEach((checkbox) => {
+        const attackSpearCheckbox = document.getElementById('attackSpear') as HTMLInputElement;
         if (checkbox.checked) {
             if(checkbox.id === 'defenseHelmet'){
                 defenderStrength += Helmet_Defense_Value;
             }else if(checkbox.id === 'defenseSpear'){
+              if(attackSpearCheckbox.checked){ /*Should only get defense value if in melee combat */
                 defenderStrength += Spear_Defense_Value;
+              }
             }else if(checkbox.id === 'defenseMounted'){
-                defenderStrength += Mounted_Defense_Value;
+              if(attackSpearCheckbox.checked){ /*defensive bonus different if in melee combat */
+                defenderStrength += Mounted_Melee_Defense_Value;
+              }else{
+                defenderStrength += Mounted_Ranged_Defense_Value;
+              }
             }
         }
       });
@@ -65,36 +103,69 @@ document.addEventListener('DOMContentLoaded', () => {
     defenderCheckboxes.forEach((checkbox) => {
       checkbox.addEventListener('click', updateNetStrength);
     });
+
+    rangeActual.addEventListener('input', updateNetStrength);
+
   });
 
 
-/* ----------------------Reapearing Elements------------------------------- */
-const rangedCheckbox = document.getElementById("attackRanged") as HTMLInputElement;
 
-const smallRangedCheckbox = document.getElementById("attackSmallGun") as HTMLInputElement;
-smallRangedCheckbox.style.display = "none"; /*hide the small ranged checkbox by default */
-const smallRangedLabel = document.querySelector('label[for="attackSmallGun"]') as HTMLLabelElement;
-smallRangedLabel.style.display = "none"; /*hide the small ranged label by default */
-const mediumRangedCheckbox = document.getElementById("attackMediumGun") as HTMLInputElement;
-mediumRangedCheckbox.style.display = "none"; /*hide the medium ranged checkbox by default */
-const mediumRangedLabel = document.querySelector('label[for="attackMediumGun"]') as HTMLLabelElement;
-mediumRangedLabel.style.display = "none"; /*hide the medium ranged label by default */
+/* ----------------------Reapearing Elements------------------------------- */
+const attackRangedCheckbox = document.getElementById("attackRanged") as HTMLInputElement;
+const defenseRangedCheckbox = document.getElementById("defenseRanged") as HTMLInputElement;
+
+const attackSmallRangedCheckbox = document.getElementById("attackSmallGun") as HTMLInputElement;
+attackSmallRangedCheckbox.style.display = "none"; /*hide the small ranged checkbox by default */
+const AttackSmallRangedLabel = document.querySelector('label[for="attackSmallGun"]') as HTMLLabelElement;
+AttackSmallRangedLabel.style.display = "none"; /*hide the small ranged label by default */
+const attackMediumRangedCheckbox = document.getElementById("attackMediumGun") as HTMLInputElement;
+attackMediumRangedCheckbox.style.display = "none"; /*hide the medium ranged checkbox by default */
+const attackMediumRangedLabel = document.querySelector('label[for="attackMediumGun"]') as HTMLLabelElement;
+attackMediumRangedLabel.style.display = "none"; /*hide the medium ranged label by default */
+
+const defenseSmallRangedCheckbox = document.getElementById("defenseSmallGun") as HTMLInputElement;
+defenseSmallRangedCheckbox.style.display = "none"; /*hide the small ranged checkbox by default */
+const defenseSmallRangedLabel = document.querySelector('label[for="defenseSmallGun"]') as HTMLLabelElement;
+defenseSmallRangedLabel.style.display = "none"; /*hide the small ranged label by default */
+const defenseMediumRangedCheckbox = document.getElementById("defenseMediumGun") as HTMLInputElement;
+defenseMediumRangedCheckbox.style.display = "none"; /*hide the medium ranged checkbox by default */
+const defenseMediumRangedLabel = document.querySelector('label[for="defenseMediumGun"]') as HTMLLabelElement;
+defenseMediumRangedLabel.style.display = "none"; /*hide the medium ranged label by default */
+
   
-rangedCheckbox.addEventListener("change", function() {  /*triggered when the ranged checkbox is changed(clicked) */
+attackRangedCheckbox.addEventListener("change", function() {  /*triggered when the ranged checkbox is changed(clicked) */
   if (this.checked) {
-    smallRangedCheckbox.style.display = "inline-block";
-    smallRangedCheckbox.checked = false;
-    smallRangedLabel.style.display = "inline-block";
-    mediumRangedCheckbox.style.display = "inline-block";
-    mediumRangedCheckbox.checked = false;
-    mediumRangedLabel.style.display = "inline-block";
+    attackSmallRangedCheckbox.style.display = "inline-block";
+    attackSmallRangedCheckbox.checked = false;
+    AttackSmallRangedLabel.style.display = "inline-block";
+    attackMediumRangedCheckbox.style.display = "inline-block";
+    attackMediumRangedCheckbox.checked = false;
+    attackMediumRangedLabel.style.display = "inline-block";
   } else {
-    smallRangedCheckbox.style.display = "none";
-    smallRangedCheckbox.checked = false;
-    smallRangedLabel.style.display = "none";
-    mediumRangedCheckbox.style.display = "none";
-    mediumRangedCheckbox.checked = false;
-    mediumRangedLabel.style.display = "none";
+    attackSmallRangedCheckbox.style.display = "none";
+    attackSmallRangedCheckbox.checked = false;
+    AttackSmallRangedLabel.style.display = "none";
+    attackMediumRangedCheckbox.style.display = "none";
+    attackMediumRangedCheckbox.checked = false;
+    attackMediumRangedLabel.style.display = "none";
+  }
+});
+
+defenseRangedCheckbox.addEventListener("change", function() {  /*triggered when the ranged checkbox is changed(clicked) */
+  if (this.checked) {
+    defenseSmallRangedCheckbox.style.display = "inline-block";
+    defenseSmallRangedCheckbox.checked = false;
+    defenseSmallRangedLabel.style.display = "inline-block";
+    defenseMediumRangedCheckbox.style.display = "inline-block";
+    defenseMediumRangedCheckbox.checked = false;
+    defenseMediumRangedLabel.style.display = "inline-block";
+  } else {
+    defenseSmallRangedCheckbox.style.display = "none";
+    defenseSmallRangedCheckbox.checked = false;
+    defenseSmallRangedLabel.style.display = "none";
+    defenseMediumRangedCheckbox.style.display = "none";
+    defenseMediumRangedCheckbox.checked = false;
+    defenseMediumRangedLabel.style.display = "none";
   }
 });
   
